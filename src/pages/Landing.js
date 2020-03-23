510a2d9db9c6b7c01bab2040c880238192ba31ad
unknown <guiilherme.bayer@gmail.com>
2020-03-23 14:02:00 -0300

    Adicionando animações de entrada

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,6 +2,8 @@ import React, { memo, useContext, useState } from 'react'
 import { Container, Col, Hidden } from 'react-grid-system'
 import styled, { ThemeContext, css } from 'styled-components'
 import { theme, ifProp } from 'styled-tools'
+import { motion } from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
 
 import peoples from '../assets/images/peoples.svg'
 
@@ -51,7 +53,7 @@ const Header = styled.div`
   `}
 `
 
-Header.Content = styled(Col)`
+Header.Content = styled(motion.div)`
   height: 100%;
   padding-top: 10%;
 
@@ -83,13 +85,13 @@ const Subtitle = styled(Text)`
   `}
 `
 
-const About = styled.div`
+const About = styled(motion.div)`
   width: 100%;
   height: 80vh;
   background-color: ${theme('colors.quartiary')};
 `
 
-const Point = styled.div`
+const Point = styled(motion.div)`
   width: 300px;
   height: 260px;
   padding: 45px;
@@ -141,10 +143,20 @@ const Explanation = styled(Text)`
   text-align: center;
 `
 
+export const enterWithY = y => ({
+  initial: { opacity: 0 },
+  enter: { opacity: 1, y },
+  exit: { opacity: 0 }
+})
+
 export const Landing = memo(() => {
   const { colors } = useContext(ThemeContext)
   const [email, setEmail] = useState('')
   const [modal, setModal] = useState(false)
+
+  const [about, inAbout] = useInView({
+    rootMargin: '-100px 0px'
+  })
 
   return (
     <>
@@ -162,42 +174,57 @@ export const Landing = memo(() => {
 
       <Header>
         <Container>
-          <Header.Content>
-            <Title color={colors.seventiary} weight="bold">
-              Traga seus usuários para o cíclo de decisão.
-            </Title>
+          <Header.Content
+            exit="exit"
+            initial="initial"
+            animate="enter"
+            variants={{
+              initial: { y: -200 },
+              enter: { transition: { staggerChildren: 0.4 } }
+            }}
+          >
+            <motion.div variants={enterWithY(200)}>
+              <Title color={colors.seventiary} weight="bold">
+                Traga seus usuários para o cíclo de decisão.
+              </Title>
+            </motion.div>
 
-            <Description
-              color={colors.support.quintiary}
-              size="eighteen"
-              weight="light"
-              height={22}
-              top={30}
-              bottom={40}
-            >
-              Que tal aproximar mais o usuário da construção do seu produto ou
-              serviço? Receba requisições de novas funcionalidades ou correções,
-              transpareça mais o que é entregue, cadastre se abaixo.
-            </Description>
+            <motion.div variants={enterWithY(200)}>
+              <Description
+                color={colors.support.quintiary}
+                size="eighteen"
+                weight="light"
+                height={22}
+                top={30}
+                bottom={40}
+              >
+                Que tal aproximar mais o usuário da construção do seu produto ou
+                serviço? Receba requisições de novas funcionalidades ou
+                correções, transpareça mais o que é entregue, cadastre se
+                abaixo.
+              </Description>
+            </motion.div>
 
-            <Email>
-              <Input
-                iconAlign="right"
-                full={true}
-                placeholder="Seu email aqui"
-                onChange={({ target }) => setEmail(target.value)}
-                value={email}
-                onClickIcon={() => setModal(true)}
-                icon={({ width, height, color }) => (
-                  <Icon
-                    name="right"
-                    width={width}
-                    height={height}
-                    color={color}
-                  />
-                )}
-              />
-            </Email>
+            <motion.div variants={enterWithY(200)}>
+              <Email>
+                <Input
+                  iconAlign="right"
+                  full={true}
+                  placeholder="Seu email aqui"
+                  onChange={({ target }) => setEmail(target.value)}
+                  value={email}
+                  onClickIcon={() => setModal(true)}
+                  icon={({ width, height, color }) => (
+                    <Icon
+                      name="right"
+                      width={width}
+                      height={height}
+                      color={color}
+                    />
+                  )}
+                />
+              </Email>
+            </motion.div>
           </Header.Content>
         </Container>
 
@@ -206,64 +233,93 @@ export const Landing = memo(() => {
         </Hidden>
       </Header>
 
-      <About>
-        <Subtitle color={colors.seventiary} weight="bold" bottom={20}>
-          Como funciona?
-        </Subtitle>
-
-        <Explanation
-          color={colors.support.quintiary}
-          size="eighteen"
-          weight="light"
-          height={22}
-          bottom={80}
+      <About ref={about}>
+        <motion.div
+          exit="exit"
+          initial="initial"
+          animate="enter"
+          variants={{
+            initial: { y: -200 },
+            enter: { transition: { staggerChildren: 0.6 } }
+          }}
         >
-          Some explanation here as it work, unde omnis iste natus error sit
-          voluptatem.
-        </Explanation>
+          <motion.div
+            variants={enterWithY(200)}
+            initial="inital"
+            animate={inAbout ? 'enter' : 'exit'}
+          >
+            <Subtitle color={colors.seventiary} weight="bold" bottom={20}>
+              Como funciona?
+            </Subtitle>
+          </motion.div>
 
-        <About.Content>
-          <Point variant="primary">
-            <Text
-              className="title"
+          <motion.div
+            variants={enterWithY(200)}
+            initial="inital"
+            animate={inAbout ? 'enter' : 'exit'}
+          >
+            <Explanation
               color={colors.support.quintiary}
-              weight="bold"
+              size="eighteen"
+              weight="light"
+              height={22}
+              bottom={80}
             >
-              Colete feedbacks
-            </Text>
+              Some explanation here as it work, unde omnis iste natus error sit
+              voluptatem.
+            </Explanation>
+          </motion.div>
 
-            <Text weight="light" color={colors.support.quintiary}>
-              Tenha uma visão clara de novas funcionalidades e correções que
-              estão sendo mais sugeridas por seus usuário em um único lugar.
-            </Text>
-          </Point>
+          <motion.div
+            variants={enterWithY(200)}
+            exit="exit"
+            initial="inital"
+            animate={inAbout ? 'enter' : 'exit'}
+          >
+            <About.Content>
+              <Point variant="primary">
+                <Text
+                  className="title"
+                  color={colors.support.quintiary}
+                  weight="bold"
+                >
+                  Colete feedbacks
+                </Text>
 
-          <Point variant="secondary">
-            <Text className="title" weight="bold" color={colors.quartiary}>
-              Defina um roadmap
-            </Text>
+                <Text weight="light" color={colors.support.quintiary}>
+                  Tenha uma visão clara de novas funcionalidades e correções que
+                  estão sendo mais sugeridas por seus usuário em um único lugar.
+                </Text>
+              </Point>
 
-            <Text weight="light" color={colors.quartiary}>
-              Deixe bem explícito para todos no que atualmente seu time está
-              trabalhando e quais serão os próximos afazeres.
-            </Text>
-          </Point>
+              <Point variant="secondary">
+                <Text className="title" weight="bold" color={colors.quartiary}>
+                  Defina um roadmap
+                </Text>
 
-          <Point variant="primary">
-            <Text
-              className="title"
-              color={colors.support.quintiary}
-              weight="bold"
-            >
-              Exponha um changelog
-            </Text>
+                <Text weight="light" color={colors.quartiary}>
+                  Deixe bem explícito para todos no que atualmente seu time está
+                  trabalhando e quais serão os próximos afazeres.
+                </Text>
+              </Point>
 
-            <Text weight="light" color={colors.support.quintiary}>
-              Anuncie os últimos lançamentos do seu produto, engajando seus
-              usuários mais cedo com novas funcionalidades e correções.
-            </Text>
-          </Point>
-        </About.Content>
+              <Point variant="primary">
+                <Text
+                  className="title"
+                  color={colors.support.quintiary}
+                  weight="bold"
+                >
+                  Exponha um changelog
+                </Text>
+
+                <Text weight="light" color={colors.support.quintiary}>
+                  Anuncie os últimos lançamentos do seu produto, engajando seus
+                  usuários mais cedo com novas funcionalidades e correções.
+                </Text>
+              </Point>
+            </About.Content>
+          </motion.div>
+        </motion.div>
       </About>
     </>
   )
